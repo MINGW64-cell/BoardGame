6efa31d03355d361a763fbb43c1cd61f8a947c3c
Steven C <73195594+MINGW64-cell@users.noreply.github.com>
2023-01-16 09:40:28 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,22 +1,16 @@
 package code;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class test extends JPanel {
 
@@ -43,17 +37,23 @@ public class test extends JPanel {
 		
 	}
 	
-	public void initButton(JButton a, String iconName, boolean mnemonic) {
+	public void initButton(JButton a, String iconName, boolean mnemonic, int x, int y) {
 		
 		if(mnemonic)	{
 			a.setMnemonic(KeyEvent.VK_P);
 		}
 		
 		a.setText(iconName);
-		die.setVisible(true);
+		a.setVisible(true);
+		a.setLocation(x, y);
 		
 	}
 
+	public void initJLabel(JLabel a, String hoverText, int xCoord, int yCoord) {
+		a.setLocation(xCoord * 39, yCoord * 39);
+		a.setToolTipText(hoverText);
+		a.setSize(80, 80);
+	}
 	
 	public static void main(String[] args) {
 
@@ -69,12 +69,12 @@ public class test extends JPanel {
 		init(base, row * 40 + 20, col * 40 + 40);
 		base.add(a);
 		base.add(content);		
-
 //Die
 		
 		JFrame diceHolder = new JFrame("Die");
-		
 		init(diceHolder, 170, 170);		
+		diceHolder.setLocation(1000, 0);
+		
 		JPanel diePanel = new JPanel();
 		diePanel.setLayout(new FlowLayout(3, 50, 50));
 		diePanel.setVisible(true);
@@ -86,10 +86,24 @@ public class test extends JPanel {
 		diceHolder.add(yourRollIs);
 		
 		JButton die = new JButton();
-		a.initButton(die, "Die", true);
+		a.initButton(die, "Die", true, 25, 25);   
 		die.setSize(15, 40);
-		die.setLocation(25, 25);
 		die.setVisible(true);
+		die.setToolTipText("Roll a virtual 6-sided die");
+
+//ITW References
+		
+		JFrame miniBoard = new JFrame();
+		init(miniBoard, 200, 200);
+		miniBoard.setLocationRelativeTo(base);
+		miniBoard.setResizable(false);
+		
+		JPanel bg = new JPanel();
+		miniBoard.add(bg);
+		
+		JTextField thing = new JTextField();
+		
+		bg.add(thing);
 		
 //Win Condition and Game Runner
 		
@@ -102,6 +116,36 @@ public class test extends JPanel {
 				yourRollIs.setText("Your Roll Is: " + rollResult);
 				content.diceSum += rollResult;
 				System.out.println(rollResult + " " + content.diceSum);
+				content.move();
+				
+				
+				int bool = content.diceSum / 10;
+			
+				if(bool == 1)	{	
+					thing.setText("McCandless was hungry and was foraging berries off of bushes. "
+							+ "Jan and Bob Burres found him, with the former being someone Chris "
+							+ "sent many post cards to.");
+				}	else if(bool == 2)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 3)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 4)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 5)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 6)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 7)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 8)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 9)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}	else if(bool == 10)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");						
+				}	else if(bool == 11)	{
+					thing.setText("Welcome to Alaska, your excusion will start here");
+				}
 				
 				if(content.diceSum >= 110)	{
 					JFrame victory = new JFrame("You Won");
@@ -113,8 +157,10 @@ public class test extends JPanel {
 					JLabel message = new JLabel();
 					message.setVisible(true);
 					message.setText("We have a winner!");
+					vic.add(message);
 					diceHolder.setVisible(false);
 					victory.setLocationRelativeTo(diceHolder);
+					victory.add(vic);
 				}
 				
 				dieRolled = true;
